71c4835c9f262ff8b43e516d1ca8107114c9286b
Iman0810 <iman.chandrasiri0810@gmail.com>
2026-06-17 20:19:57 +0530

    Metric controller added

---
+++ src/main/java/backend/controller/MetricController.java
@@ -0,0 +1,40 @@
+package backend.controller;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import backend.model.Metric;
+import backend.repository.MetricRepository;
+
+
+
+@RestController
+@RequestMapping("/api/metrics")
+public class MetricController {
+
+    private final MetricRepository metricRepository;
+
+    public MetricController(MetricRepository metricRepository) {
+        this.metricRepository = metricRepository;
+    }
+
+    @PostMapping
+    public Metric createMetric(@RequestBody Metric metric){
+        metric.setTimestamp(LocalDateTime.now());
+        return metricRepository.save(metric);
+    }
+
+    @GetMapping
+    public List<Metric> getMetrics(){
+        return metricRepository.findAll();
+    }
+   
+    
+
+}
